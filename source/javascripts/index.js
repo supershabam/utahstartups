bfd47fb0a4182ae1452d3884f7e5de4b87a0b773
Ian Hansen <ian@supershabam.com>
2014-01-08 21:38:59 -0700

    map options

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -14,7 +14,8 @@
     }
   ]
 
-  var map = L.map('map').setView([40.7607793, -111.891047], 9)
+  var map = L.map('map', {
+  }).setView([40.7607793, -111.891047], 9)
   L.tileLayer('http://{s}.tiles.mapbox.com/v3/supershabam.gp32cc1l/{z}/{x}/{y}.png').addTo(map)
   entries.forEach(function(entry) {
     L.marker(entry.loc)
