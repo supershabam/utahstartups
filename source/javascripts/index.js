06197c7abe98a293a338a0287334b50cd1bad2a9
Ian Hansen <ian@supershabam.com>
2014-01-08 20:40:05 -0700

    full map

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -1,2 +1,2 @@
-var map = L.map('map').setView([40.7607793, -111.891047], 13)
+var map = L.map('map').setView([40.7607793, -111.891047], 9)
 L.tileLayer('http://{s}.tiles.mapbox.com/v3/supershabam.gp32cc1l/{z}/{x}/{y}.png').addTo(map)
