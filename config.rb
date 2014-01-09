33c7c72a125bdd267dcb80df97b1d43caaa7e367
Ian Hansen <ian@supershabam.com>
2014-01-08 21:36:40 -0700

    adds spacemonkey

--- config.rb
+++ config.rb
@@ -5,20 +5,3 @@ set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
 
-class BuildEntries < Middleman::Extension
-  def initialize(app, options_hash={}, &block)
-    super
-    app.after_build do |builder|
-      builder.create_file "build/test.json" do
-        json = {
-          "test" => "value"
-        }
-        json.to_s
-      end
-    end
-  end
-end
-
-::Middleman::Extensions.register(:build_entries, BuildEntries)
-
-activate :build_entries
--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -1,2 +1,24 @@
-var map = L.map('map').setView([40.7607793, -111.891047], 9)
-L.tileLayer('http://{s}.tiles.mapbox.com/v3/supershabam.gp32cc1l/{z}/{x}/{y}.png').addTo(map)
+(function() {
+  'use strict'
+
+  var entries = [
+    {
+      loc: [40.6110589, -111.8999352],
+      popup: [
+        "#space monkey",
+        "",
+        "Space Monkey is taking the cloud out of the datacenter creating a faster, cheaper, and more durable way to store and access data, from any device.",
+        "",
+        "[http://spacemonkey.com](http://spacemonkey.com)"
+      ].join("\n")
+    }
+  ]
+
+  var map = L.map('map').setView([40.7607793, -111.891047], 9)
+  L.tileLayer('http://{s}.tiles.mapbox.com/v3/supershabam.gp32cc1l/{z}/{x}/{y}.png').addTo(map)
+  entries.forEach(function(entry) {
+    L.marker(entry.loc)
+      .bindPopup(marked(entry.popup))
+      .addTo(map)
+  })
+}())
