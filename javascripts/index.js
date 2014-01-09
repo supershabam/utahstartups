1dfaaf95df2584a45b69eaf0cb6b89355247a6f5
Ian Hansen <ian@supershabam.com>
2014-01-08 21:48:41 -0700

    Site updated to dc82ef1

--- javascripts/index.js
+++ javascripts/index.js
@@ -4,17 +4,28 @@
   var entries = [
     {
       loc: [40.6110589, -111.8999352],
-      popup: [
-        "#space monkey",
-        "",
-        "Space Monkey is taking the cloud out of the datacenter creating a faster, cheaper, and more durable way to store and access data, from any device.",
-        "",
-        "[http://spacemonkey.com](http://spacemonkey.com)"
-      ].join("\n")
+      popup: "[Space Monkey](http://spacemonkey.com)"
+    },
+    {
+      loc: [40.5548965,-111.9147611],
+      popup: "[Hire Vue](http://hirevue.com/)"
+    },
+    {
+      loc: [40.5282631,-111.8823318],
+      popup: "[Lucid chart](https://www.lucidchart.com/)"
+    },
+    {
+      loc: [40.301899,-111.6586304],
+      popup: "[MoneyDesktop](http://moneydesktop.com/)"
+    },
+    {
+      loc: [40.3614467,-111.7817695],
+      popup: "[DropShip](http://dropship.com/)"
     }
   ]
 
-  var map = L.map('map').setView([40.7607793, -111.891047], 9)
+  var map = L.map('map', {
+  }).setView([40.7607793, -111.891047], 9)
   L.tileLayer('http://{s}.tiles.mapbox.com/v3/supershabam.gp32cc1l/{z}/{x}/{y}.png').addTo(map)
   entries.forEach(function(entry) {
     L.marker(entry.loc)
